a5e39f159c69ae1bc58c80263b37a73c5a3e57b6
Shaif Ali <mdshaifali990@gmail.com>
2026-09-14 04:36:09 +0530

    basic 2

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+const myarr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["ironman", "superman", "spiderman" ] 
+const InHero = ["shaktiman", "daku", "falyingjatt"]
+
+// console.log(typeof myarr); // object
+// console.log(myarr[1]);
+// console.log(myHeros);
+
+
+// Array methods
+
+// myarr.push(6);
+// myarr.push(10)
+// myarr.push(9);
+// myarr.pop(); // pop remove the last value
+
+
+// myarr.unshift(9);
+// myarr.shift()
+// console.log(myarr);
+
+
+// console.log(myarr.includes(5)); // true
+// console.log(myarr.includes(9)); // false
+
+
+
+const newarr = myarr.join() // remove bracket
+// console.log(myarr);
+// console.log(newarr);
+
+
+// slice , splice
+
+console.log("A ", myarr)
+const myn1 = myarr.slice(1,3)
+console.log(myn1);
+console.log("B ", myarr);
+
+
