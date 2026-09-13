6e34eff508fdcce61ae7a4d27f42299f336b4b7a
Shaif Ali <mdshaifali990@gmail.com>
2026-09-14 03:44:27 +0530

    Math and Num

---
+++ Basic/07_math-num.js
@@ -0,0 +1,34 @@
+const score = 400;
+const balance = new Number(10000)
+//console.log(balance.toFixed(2)); // "1000.00"
+
+// const otherNumber = 123.896
+// console.log(otherNumber.toPrecision(4)); // "123.9"
+
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-US'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ************************ Math Object ************************
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.max(1, 2, 3, 4, 5));
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*100) + 1);
+
+
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
